de3303d6e0529ca0f325b4a0a49d2377004a523a
Wellington Henrique <wellingtonhlc@gmail.com>
2022-04-28 15:52:41 -0300

    Adding Utils class

---
+++ CNPJ_Finder/Controllers/Utils.cs
@@ -0,0 +1,16 @@
+﻿using CNPJ_Finder.Entities;
+using Newtonsoft.Json;
+
+namespace CNPJ_Finder.Controllers
+{
+    internal class Utils
+    {
+        #region Convert Json to Object
+
+        public static Enterprise JsonToObject(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<Enterprise>(jsonString);
+        }
+        #endregion
+    }
+}
