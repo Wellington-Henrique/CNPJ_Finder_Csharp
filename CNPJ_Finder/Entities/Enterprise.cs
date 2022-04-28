977a8ddd6d5af5ee371e1c021693edd1f751a2ab
Wellington Henrique <wellingtonhlc@gmail.com>
2022-04-28 15:51:00 -0300

    Adding Enterprise class scope

---
+++ CNPJ_Finder/Entities/Enterprise.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CNPJ_Finder.Entities
+{
+    internal class Enterprise
+    {
+        public string CNPJ { get; set; }
+        public string Fantasia { get; set; }
+        public double Capital_social { get; set; }
+
+        public Enterprise()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $" CNPJ: {CNPJ}, \n Nome Fantasia: {Fantasia}, \n Capital Inicial: {Capital_social.ToString("F2")}";
+        }
+    }
+}
