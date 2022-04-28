18aa2e1842b81193bef5c978beec5b237febd76b
Wellington Henrique <wellingtonhlc@gmail.com>
2022-04-28 15:48:57 -0300

    Initial commit

---
+++ CNPJ_Finder/Program.cs
@@ -0,0 +1,19 @@
+﻿using CNPJ_Finder.Entities;
+using CNPJ_Finder.Controller;
+
+Enterprise enterprise = new Enterprise();
+var json = "";
+
+Console.Write("Entre com o CNPJ: ");
+string cnpj = Console.ReadLine().Replace(@"-", "").Replace(@"/", "").Replace(@".", "");
+
+using (var client = new HttpClient())
+{
+    var endpoint = new Uri($"https://www.receitaws.com.br/v1/cnpj/{cnpj}");
+    var result = client.GetAsync(endpoint).Result;
+    json = result.Content.ReadAsStringAsync().Result;
+    enterprise = Utils.JsonToObject(json);
+}
+
+
+Console.WriteLine(enterprise.ToString());
