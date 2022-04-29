976ce22fbc71f31dfedc584fe54f1536bfabc352
Wellington Henrique <wellingtonhlc@gmail.com>
2022-04-29 08:43:28 -0300

    Refactoring the program class to repeat the query

--- CNPJ_Finder/Program.cs
+++ CNPJ_Finder/Program.cs
@@ -3,17 +3,35 @@ using CNPJ_Finder.Controllers;
 
 Enterprise enterprise = new Enterprise();
 var json = "";
+char searchMore = 'y';
 
-Console.Write("Entre com o CNPJ: ");
-string cnpj = Console.ReadLine().Replace(@"-", "").Replace(@"/", "").Replace(@".", "");
-
-using (var client = new HttpClient())
+while(searchMore.Equals('y'))
 {
-    var endpoint = new Uri($"https://www.receitaws.com.br/v1/cnpj/{cnpj}");
-    var result = client.GetAsync(endpoint).Result;
-    json = result.Content.ReadAsStringAsync().Result;
-    enterprise = Utils.JsonToObject(json);
+    Console.Clear();
+    Console.Write("Entre com o CNPJ: ");
+    string cnpj = Console.ReadLine().Replace(@"-", "").Replace(@"/", "").Replace(@".", "");
+
+    using (var client = new HttpClient())
+    {
+        var endpoint = new Uri($"https://www.receitaws.com.br/v1/cnpj/{cnpj}");
+        var result = client.GetAsync(endpoint).Result;
+        json = result.Content.ReadAsStringAsync().Result;
+        enterprise = Utils.JsonToObject(json);
+    }
+    
+    Console.WriteLine();
+
+    if (enterprise.Status == "ERROR")
+        Console.WriteLine("CNPJ não encontrado!");
+    else
+        Console.WriteLine(enterprise.ToString());
+
+    Console.WriteLine();
+    Console.Write("Deseja consultar um novo CNPJ (Y/n)? ");
+    searchMore = char.Parse(Console.ReadLine().ToLower());
 }
 
-Console.WriteLine();
-Console.WriteLine(enterprise.ToString());
+
+
+
+        
